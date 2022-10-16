727859587d81c00dc558f91d546d995ccd9e2c63
savi003 <thomasavio003@gmail.com>
2022-10-16 09:25:18 +0530

    Company Website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ServicesComponent } from './services/services.component';
+import{HttpClientModule}from '@angular/common/http';
+import { RouterModule,Routes } from '@angular/router';
+const appRoute:Routes=[
+  {path:'',component:HomeComponent},
+  {path:'Home',component:HomeComponent},
+  {path:'About',component:AboutComponent},
+  {path:'Contact',component:ContactComponent},
+  {path:'Services',component:ServicesComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    ServicesComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
